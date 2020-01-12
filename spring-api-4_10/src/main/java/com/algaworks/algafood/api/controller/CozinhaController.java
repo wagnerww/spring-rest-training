4a2c7c26ea6532a85b069ab8cc0f6888ccbf9920
wagnerww <wagnerricardonet@gmail.com>
2020-01-12 13:30:09 -0300

    add path variable

--- spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,9 @@ public class CozinhaController {
 		public List<Cozinha> listar(){
 			return cozinhaRepository.listar();
 		}
-	
+		
+		@GetMapping("/{cozinhaId}")
+		public Cozinha buscar(@PathVariable Long cozinhaId) {
+			return cozinhaRepository.buscar(cozinhaId);
+		}
 }
