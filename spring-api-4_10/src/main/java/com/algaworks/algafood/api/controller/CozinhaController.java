ffcd828f2aa372b8a62de2ea7976f326ee1aa092
wagnerww <wagnerricardonet@gmail.com>
2020-01-13 22:26:56 -0300

    wrapper xml

--- spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,12 +3,16 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
@@ -20,13 +24,29 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
-		@GetMapping(produces = 	{MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+		@ResponseStatus(value = HttpStatus.OK)
+		@GetMapping(produces = 	{MediaType.APPLICATION_JSON_VALUE})
 		public List<Cozinha> listar(){
 			return cozinhaRepository.listar();
 		}
 		
+		@GetMapping(produces = 	{MediaType.APPLICATION_XML_VALUE})
+		public CozinhasXmlWrapper listaXmlr(){
+			return new CozinhasXmlWrapper(cozinhaRepository.listar());
+		}
+		
+		
 		@GetMapping("/{cozinhaId}")
-		public Cozinha buscar(@PathVariable Long cozinhaId) {
-			return cozinhaRepository.buscar(cozinhaId);
+		public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+			// return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+			
+			if(cozinha != null) {
+				return ResponseEntity.ok(cozinha);
+			}
+			
+			//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			return ResponseEntity.notFound().build();
+			
 		}
 }
--- spring-api-4_10/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ spring-api-4_10/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -7,11 +7,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -23,6 +28,8 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	// @JsonIgnore
+	@JsonProperty(value = "titulo")
 	@Column(name="nome", nullable = false)	
 	private String nome;
 	
