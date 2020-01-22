41feadec960f85f00f97216770a49c6a7f237342
wagnerww <wagnerricardonet@gmail.com>
2020-01-22 09:43:44 -0300

    repair imports

--- spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -2,11 +2,9 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
