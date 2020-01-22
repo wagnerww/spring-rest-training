aef000a55943ad7d52a6186a02ac960e7bfc61f9
wagnerww <wagnerricardonet@gmail.com>
2020-01-21 21:28:30 -0300

    create at post restaurant

--- spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
@@ -23,6 +25,7 @@ public class RestauranteController {
 	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+	
 	
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
@@ -46,9 +49,18 @@ public class RestauranteController {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Restaurante adicionar(@RequestBody Restaurante restaurante){
-		return cadastroRestaurante.salvar(restaurante);
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante){
+		try {
+			restaurante = cadastroRestaurante.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+		
+		
 	}
 	
 }
