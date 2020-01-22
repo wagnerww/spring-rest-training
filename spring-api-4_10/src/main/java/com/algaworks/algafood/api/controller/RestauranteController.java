ac014dcf1b48bf9cae5b63d6265eeef194099164
wagnerww <wagnerricardonet@gmail.com>
2020-01-21 21:46:55 -0300

    update restaurant

--- spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ spring-api-4_10/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -58,9 +59,26 @@ public class RestauranteController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
-		
-		
-		
+	}
+	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante){
+		try {
+			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+			
+			if(restauranteAtual!=null) {
+				restauranteAtual.setNome(restaurante.getNome());
+				restauranteAtual.setCozinha(restaurante.getCozinha());
+				
+				cadastroRestaurante.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
+				
+			}
+			
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 	
 }
