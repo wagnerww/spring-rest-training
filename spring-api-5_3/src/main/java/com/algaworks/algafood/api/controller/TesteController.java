b28ffe1e6e02e8bca66b1ba44887cd198e209c93
wagnerww <wagnerricardonet@gmail.com>
2020-01-29 13:21:23 -0300

    training other methos of query params

--- spring-api-5_3/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ spring-api-5_3/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -35,6 +35,11 @@ public class TesteController {
 		return cozinhaRepository.findByNome(nome);
 	}
 	
+	@GetMapping("/cozinhas/exists-nome")
+	public boolean cozinhaExisteNome(@RequestParam("nome") String nome){
+		return cozinhaRepository.existsByNome(nome);
+	}
+	
 	@GetMapping("/restaurantes/por-taxa-frete")
 	public List<Restaurante> restaurantePorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal){
 		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
@@ -44,4 +49,20 @@ public class TesteController {
 	public List<Restaurante> restaurantePorNome(String nome, Long cozinhaId){
 		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
 	}
+	
+	@GetMapping("/restaurantes/primeiro-por-nome")
+	public Optional<Restaurante> restaurantePorNome(String nome){
+		return restauranteRepository.findFirstByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/top2-por-nome")
+	public List<Restaurante> restauranteTopPorNome(String nome){
+		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/count-por-cozinha")
+	public int restauranteCountPorCozinha(Long cozinhaId){
+		return restauranteRepository.countByCozinhaId(cozinhaId);
+	}
+	
 }
--- spring-api-5_3/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ spring-api-5_3/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -15,5 +16,11 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	
+	Optional<Restaurante> findFirstByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId(Long cozinha);
 
 }
