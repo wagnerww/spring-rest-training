b01ef2f16508c2d0f731385c7771f59fd633e673
wagnerww <wagnerricardonet@gmail.com>
2020-02-01 11:26:29 -0300

    creating timestamps

--- spring-api-5_18/src/main/resources/import.sql
+++ spring-api-5_18/src/main/resources/import.sql
@@ -1,8 +1,8 @@
 insert into cozinha (id, nome) values (1, 'Tailandeza');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Gulz', 9.75, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tio Arno', 10, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ('Gulz', 9.75, 1, utc_timestamp, utc_timestamp);
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ('Tio Arno', 10, 2, utc_timestamp, utc_timestamp);
 
 insert into estado (id, nome) values (1, 'Rio Grande do Sul');
 insert into estado (id, nome) values (2, 'Santa Catarina');
