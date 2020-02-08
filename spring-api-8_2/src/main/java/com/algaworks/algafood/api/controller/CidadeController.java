59e4c65e4b09870d9909bab721030c805ed8a3be
wagnerww <wagnerricardonet@gmail.com>
2020-02-08 18:02:58 -0300

    exception handlers

--- spring-api-8_2/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ spring-api-8_2/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.exceptionhandler.Problema;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
@@ -77,4 +81,6 @@ public class CidadeController {
 	public void remover(@PathVariable Long cidadeId){
 		cadastroCidade.excluir(cidadeId);		
 	}
+	
+	
 }
