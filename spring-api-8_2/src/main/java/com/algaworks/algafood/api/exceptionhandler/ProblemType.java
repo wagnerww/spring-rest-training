ff3da29fd66ff725c44b68c201d76e35ebeecbb5
wagnerww <wagnerricardonet@gmail.com>
2020-02-11 13:31:46 -0300

    initialize bean validation

--- spring-api-8_2/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ spring-api-8_2/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
-	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 	
